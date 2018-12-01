27c6345156d541518edef9d871c79c848a3dd79b
zongtong009 <zongtongguilai1@gmail.com>
2018-12-02 01:24:10 +0800

    update1202

---
+++ python123/web-crawler/001.py
@@ -0,0 +1,14 @@
+# from requests_html import HTMLSession
+from lxml import etree
+# html = '''
+# <div class="content">
+#   <h3><b>理解网页结构</b></h3>
+#   <p>我们所看到的所有网站都是由 <b>结构化</b> 的代码构成的，经过浏览器的处理，呈现出各式各样的页面。</p>
+#   <p>一个网页通常由三部分代码组成：HTML 代码、CSS 代码 和 Javascript 代码。</p>
+#   <p>下面的文本框中是现在你看到的这段文字背后的代码，尝试进行以下操作：</p>
+# </div>
+# '''
+#
+# session = HTMLSession()
+# r = session.get('https://movie.douban.com/subject/1292052/')
+# print(r.text)
--- youdao2.py
+++ youdao2.py
@@ -1,20 +1,57 @@
-from tkinter import *
-from tkinter import messagebox
-import re
+# if __name__ == "__main__":
+#     main()
+import tkinter as tk
 import requests
 
+window = tk.Tk()
+window.title("主人的翻译器")
+window.geometry("550x200+300+270")  # 长乘高
 
-# 模拟浏览器发送请求
+'''label标签栏'''
+l0 = tk.Label(window, text="输入内容", font="微软雅黑 15", height=2)
+l0.grid()
+l1 = tk.Label(window, text="翻译", font="微软雅黑 15", height=2)
+l1.grid()
+# l2 = tk.Label(window, height=2)
+# l2.grid()
+
+var = tk.StringVar()
+'''text文本框'''
+e = tk.Entry(window, width=50)
+e.grid(row=0, column=1)
+e1 = tk.Entry(window, textvariable=var, width=50)
+e1.grid(row=1, column=1)
 
 
-def main():
-    root = Tk()
-    root.title('那真的牛啤')
-    root.geometry('600x300')
-    root.geometry('-200+200')
-    label = Label(root, text='233')
-    label.grid(row=0, column=0)
+def click():
+    content = e.get()
+    if content == '':
+        return
+    data = {
+        "i": content,
+        "from": "AUTO",
+        "to": "AUTO",
+        "smartresult": "dict",
+        "client": "fanyideskweb",
+        "doctype": "json",
+        "version": "2.1",
+        "keyfrom": "fanyi.web",
+        "action": "FY_BY_REALTIME",
+        "typoResult": "false"
+    }
+    response = requests.post(
+        "http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule", data=data).json()
+    bb = response["translateResult"][0][0]["tgt"]
+    # print(bb)
+    # print(type(bb))
+    var.set(bb)
 
 
-if __name__ == "__main__":
-    main()
+b0 = tk.Button(window, text="点击翻译", command=click, width=15, font="微软雅黑 12")
+b0.grid()
+b1 = tk.Button(window, text="退出", command=window.quit, width=15, font="微软雅黑 12")
+b1.grid(row=2, column=1)
+# b2 = tk.Button(window, width=15, font="微软雅黑 12")
+# b2.grid(row=2, column=1)
+
+window.mainloop()
