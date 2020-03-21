bf227110f61cda387aa5ff987334551c7e505b2e
zongtong009 <zongtongguilai1@gmail.com>
2020-03-21 12:14:05 +0800

    Update 00test.py

--- 00test.py
+++ 00test.py
@@ -1,9 +1,15 @@
 """
 本文件不具备保存意义，可以随意更改
 """
-print()
-a = (1 in [1, 2, 3])
-print(a)
+# print()
+# a = (1 in [1, 2, 3])
+# print(a)
 
-for i in range(1100):
-    print(i)
+# for i in range(1100):
+#     print(i)
+# import copy
+# a=[[1,2],2,[5,6]]
+
+# a[0][1]=34
+# print(a)
+import numpy as np
